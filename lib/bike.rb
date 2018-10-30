742787504ea6bc1d2e647a7760dc7cbb40c3db71
topickering <topickering@gmail.com>
2018-10-30 17:17:10 +0000

    further tests

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,7 @@
 class Bike
 
   def working?
+    self
   end
-  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,20 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+end
+
+describe DockingStation do
+  it 'is expected to release a Bike' do
+  expect(subject.release_bike).to be_instance_of(Bike)
+end
+end
+
+describe Bike do
+  it { is_expected.to respond_to(:working?) }
+end
+
+describe Bike do
+  it { is_expected.to be_working }
 end
