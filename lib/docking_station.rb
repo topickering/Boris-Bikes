df9f5819792d835b10a865fb34741c04dee31f09
topickering <topickering@gmail.com>
2018-10-31 17:27:51 +0000

    exepcted error if station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,21 @@ require_relative 'bike'
 
 class DockingStation
 
-attr_reader :bike
+attr_reader :bike, :capacity
+
+  def initialize
+    @capacity = 0
+  end
 
   def dock(bike)
+    fail 'Station is full' unless @capacity < 1
+    @capacity += 1
     @bike = bike
   end
 
   def release_bike
     fail 'Station is empty' unless @bike
+    @capacity -= 1
     @bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,24 +5,18 @@ describe DockingStation do
 end
 
 describe DockingStation do
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-end
-
-describe DockingStation do
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
-end
-
-describe DockingStation do
   describe '#release_bike' do
     it 'returns an error if there are no bikes' do
       expect { subject.release_bike }.to raise_error 'Station is empty'
     end
   end
+
+  describe '#dock(bike)' do
+    it 'returns an error if the station is full' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect { subject.dock(bike) }.to raise_error 'Station is full'
+    end
+  end
+
 end
