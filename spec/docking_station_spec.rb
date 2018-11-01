5808ffe9b0d0ff45a5b269a63d56784f6e2c83d8
topickering <topickering@gmail.com>
2018-11-01 15:44:14 +0000

    Adding ability to set variable capacity with a default

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-end
 
-describe DockingStation do
   describe '#release_bike' do
     it 'returns an error if there are no bikes' do
       expect { subject.release_bike }.to raise_error 'Station is empty'
@@ -15,6 +13,27 @@ describe DockingStation do
     it 'returns an error if the station is full' do
       subject.capacity.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Station is full'
+    end
+  end
+
+  describe '#new' do
+    it 'takes one argument' do
+      expect(DockingStation).to respond_to(:new).with(1).argument
+    end
+  end
+
+  it 'has a default capacity if no other is specified' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      subject.capacity.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Station is full'
     end
   end
 
